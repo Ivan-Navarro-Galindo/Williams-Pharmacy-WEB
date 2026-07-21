a6834a2f2054e892c0836f08875089683939d84b
ivann <ivanavgal@alu.edu.gva.es>
2026-07-21 08:58:27 +0200

    cambios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
         />
         <a
           href="#main"
-          className="focus:bg-navy-800 sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[60] focus:rounded-md focus:px-4 focus:py-2 focus:text-white"
+          className="focus:bg-navy-800 sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-60 focus:rounded-md focus:px-4 focus:py-2 focus:text-white"
         >
           Skip to content
         </a>
